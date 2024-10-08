89e5525a363e483ec4e71b3f842cb030c954052b
Shams  Kadri <quadrishams342@gmail.com>
2024-10-08 03:06:41 -0400

    fixing naming issue, still here not sure if fixed or not

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import App from './pages/App.js'
-import AdminLogin from './pages/AdminLogin.js'
-import AdminPanel from './pages/AdminPanel.js'
-import { AdminProvider } from './context/AdminContext.js'
+import App from './pages/App.jsx'
+import AdminLogin from './pages/AdminLogin.jsx'
+import AdminPanel from './pages/AdminPanel.jsx'
+import { AdminProvider } from './context/AdminContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
