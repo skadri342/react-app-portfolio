b8049b96b8e243003d0a36dc8c3dd802da3b24df
Shams  Kadri <quadrishams342@gmail.com>
2024-10-08 02:27:43 -0400

    updated frontend url

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connectDB();
 // Middleware
 app.use(cors({
   origin: process.env.NODE_ENV === 'production'
-    ? 'https://react-app-portfolio.onrender.com'
+    ? 'https://react-app-portfolio-frontend.onrender.com'
     : 'http://localhost:5173', // Replace with your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'],
